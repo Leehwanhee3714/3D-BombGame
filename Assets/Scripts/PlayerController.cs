0262243224f1c9932cdbadf6a4d8547088b775c8
Leehwanhee3714 <80015332+Leehwanhee3714@users.noreply.github.com>
2021-05-11 12:05:21 +0900

    2021.5.11

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float translation; 
+    private float rotation; 
+    void Update()
+    {
+        translation = Input.GetAxis("Vertical") * 10f;
+        rotation = Input.GetAxis("Horizontal") * 10f;
+
+        transform.Translate(0, 0, translation * Time.deltaTime);
+        transform.Rotate(0, rotation, 0 * Time.deltaTime);
+    }
+}
